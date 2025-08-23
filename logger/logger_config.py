7a0f027bedce3940e41df3ba699dfa28a425a7b1
marguskaen <margus.kaen@gmail.com>
2025-08-23 15:49:31 +0300

    modified logger config

--- logger/logger_config.py
+++ logger/logger_config.py
@@ -9,5 +9,10 @@ logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 logger_handler = logging.FileHandler(LOGGER_LOCATION)
 formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+console_handler = logging.StreamHandler()
+console_handler.setFormatter(formatter)
+logger.addHandler(console_handler)
+
 logger_handler.setFormatter(formatter)
 logger.addHandler(logger_handler)
