bd784cf40db25ac76bb66a0c1d9c5b4b2f38dc92
Margus Käen <margus.kaen@gmail.com>
2026-05-18 12:46:37 +0300

    added frontend url to env

--- src/auth/routes.py
+++ src/auth/routes.py
@@ -2,7 +2,11 @@ from flask_cors import CORS
 
 from src.api.user.controller import user_blueprint
 from src.api.book.controller import book_blueprint
+from dotenv import load_dotenv
+import os
 
+load_dotenv()
+FRONTEND_URL = os.getenv('FRONTEND_URL')
 
-CORS(user_blueprint, supports_credentials=True, origins=["http://127.0.0.1:8080"])
-CORS(book_blueprint, supports_credentials=True, origins=["http://127.0.0.1:8080"])
+CORS(user_blueprint, supports_credentials=True, origins=[FRONTEND_URL])
+CORS(book_blueprint, supports_credentials=True, origins=[FRONTEND_URL])
